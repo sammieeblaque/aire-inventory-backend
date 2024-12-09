a82b960d56cd542da37ddd5461a4352657b77a4b
Sammiee <88675348+sammieeblaque@users.noreply.github.com>
2024-12-09 19:46:40 +0100

    chore: added compression & helmet

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor } from '@nestjs/common';
+import helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const reflector = app.get(Reflector);
+
+  app.use(helmet());
+  app.use(compression());
 
   app.enableCors({
     origin: '*',
