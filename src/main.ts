2adb7a1ffd1c0c47a14a42691adf03c0c9edc3ef
Sammiee <88675348+sammieeblaque@users.noreply.github.com>
2024-12-09 19:39:52 +0100

    chore: added reflector

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const reflector = app.get(Reflector);
 
   app.enableCors({
     origin: '*',
   });
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
 
   await app.listen(3000);
 }
