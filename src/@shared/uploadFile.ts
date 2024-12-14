0ce80751a46979417ad5a152a35f30e87d29c6bf
Sammiee <88675348+sammieeblaque@users.noreply.github.com>
2024-12-14 19:32:01 +0100

    chore: installed papaparse types

--- src/@shared/uploadFile.ts
+++ src/@shared/uploadFile.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { parse } from 'papaparse';
+import { parse, ParseConfig } from 'papaparse';
 
 @Injectable()
 export class UploadsService<T> {
-  async uploadProductFile(filePath: string | Buffer): Promise<T[]> {
-    const csvData = filePath.toString();
+  async uploadProductFile(
+    filePath: string | Buffer,
+    options: ParseConfig = {},
+  ): Promise<T[]> {
+    const csvData =
+      typeof filePath === 'string' ? filePath : filePath.toString();
 
     return new Promise((resolve, reject) => {
       parse(csvData, {
         header: true, // Treat the first row as headers
         skipEmptyLines: true, // Ignore empty lines
-        complete: (results) => {
-          resolve(results.data); // Resolve with parsed data
-        },
-        error: (error) => {
-          reject(error); // Reject if parsing fails
-        },
+        ...options,
+        complete: (results) => resolve(results.data), // Resolve with parsed data
+        error: (error) => reject(error), // Reject if parsing fails
       });
     });
   }
