45708789a9451112ebeeb34786d2df0a9bed8035
Sammiee <88675348+sammieeblaque@users.noreply.github.com>
2025-03-21 13:30:00 +0100

    chore: updated typeorm

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -10,12 +10,17 @@ dotenvConfig();
 
 const typeorm = {
   type: 'postgres',
-  url: process.env.DATABASE_URL,
+  host: process.env.TYPEORM_POSTGRES_HOST,
+  port: parseInt(process.env.TYPEORM_POSTGRES_PORT, 10),
+  username: process.env.TYPEORM_POSTGRES_USER,
+  password: process.env.TYPEORM_POSTGRES_PASSWORD,
+  database: process.env.TYPEORM_POSTGRES_DB,
+
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/**/*{.ts,.js}'],
   retryAttempts: 2,
   retryDelay: 3000,
-  ssl: true,
+  // ssl: true,
   autoLoadEntities: true,
   logging: true,
   keepConnectionAlive: true,
--- src/modules/users/usecases/createUser.usecase.ts
+++ src/modules/users/usecases/createUser.usecase.ts
@@ -3,7 +3,6 @@ import { UseCase } from 'src/@types';
 import { User } from '../entities/user.entity';
 import { UsersService } from '../users.service';
 import { CreateUserDto } from '../dto/create-user.dto';
-import { EntityManager } from 'typeorm';
 
 @Injectable()
 export class CreateUserUsecase extends UseCase {
@@ -13,10 +12,7 @@ export class CreateUserUsecase extends UseCase {
     super();
   }
 
-  async execute(
-    entityManager: EntityManager,
-    createUserDto: CreateUserDto,
-  ): Promise<User> {
+  async execute(createUserDto: CreateUserDto): Promise<User> {
     const user = await this.userService.create(createUserDto);
     return user;
   }
